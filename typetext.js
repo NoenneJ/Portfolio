572ddd156725f654800ed5e61110d0a0ce5c19a3
NoenneJ <noenne@live.dk>
2023-10-17 11:16:54 +0200

    first commit

---
+++ typetext.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".typetext", {
+    strings: ["UX/UI design", "Project planning", "Research", "Ideation", "Sketching", "Prototyping", "Graphic design"],
+    typeSpeed: 100,
+    backSpeed: 120,
+    loop: true
+})
+
